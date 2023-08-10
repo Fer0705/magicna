71357aa94efa45438faecc59f18e4e4e28f7ea31
FerMortaro <mortarofernanda@gmail.com>
2023-08-10 18:54:12 -0300

    add image

--- magic-na/src/components/Tarot.jsx
+++ magic-na/src/components/Tarot.jsx
@@ -1,6 +1,6 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
-import { Flex, Text, Box } from "@chakra-ui/react";
+import { Flex, Text, Box, Image } from "@chakra-ui/react";
 import { Link } from "@chakra-ui/react";
 const Tarot = () => {
   return (
@@ -17,35 +17,61 @@ const Tarot = () => {
         Tarot
       </Text>
       <Box p={2} textAlign="center" bg={"blackAlpha.800"} borderRadius={"5%"}>
-        <Text
-          color={"white"}
-          textAlign="center"
-          fontSize={{ base: "lg", lg: "md" }}
-          lineHeight="1.6"
-          mt={4}
-          px={8}
+        <Flex
+          direction={{ base: "column-reverse", md: "row-reverse" }}
+          justifyContent="center" // Centra horizontalmente en pantallas grandes
+          alignItems="center"
+          mx={{ base: 2, md: 0 }}
         >
-          Soy Natalia, tarotista y guía espiritual con años de experiencia en el
-          mundo del tarot. Mi pasión es ayudarte a encontrar la claridad que
-          necesitas para enfrentar situaciones, tomar decisiones y caminar con
-          confianza hacia tu destino. Con cada pregunta que hagas, te brindaré
-          interpretaciones precisas y significativas basadas en la energía de
-          los arcanos. Además, tus Cafecitos apoyarán este espacio para que
-          pueda seguir ofreciendo contenido y consejos útiles en temas de
-          desarrollo personal, espiritualidad y crecimiento interior. <br />
-          {""}
-          <br />
-          Mi formación se centra en el Tarot Evolutivo una poderosa herramienta
-          que combina la antigua sabiduría del tarot con un enfoque
-          contemporáneo hacia el crecimiento personal y espiritual. Este estilo
-          de tarot va más allá de las predicciones simples y ofrece una
-          experiencia de lectura más profunda y significativa. Su enfoque en la
-          responsabilidad, la intuición y la autorreflexión lo convierten en una
-          opción única para aquellos que buscan un mayor entendimiento de sí
-          mismos y su camino en la vida. ¡Estoy aquí para ti! Atrévete a dar el
-          primer sorbo de sabiduría, y juntos descubriremos el poder que el
-          tarot puede tener en tu vida.
-        </Text>
+          <Box
+            order={{ base: 1, md: 2 }}
+            mt={{ base: 4, md: 0 }}
+            flex={{ base: "1", md: "initial" }}
+            display="flex"
+            justifyContent="center" // Centra horizontalmente la imagen
+            alignItems="center" // Centra verticalmente la imagen
+            minWidth="200px" // Ancho mínimo para evitar achicamiento
+            maxW="50%" // Ancho máximo para evitar que la imagen sea demasiado grande
+          >
+            <Image
+              borderRadius="full"
+              boxSize="200px"
+              src="https://res.cloudinary.com/dldclv2km/image/upload/c_thumb,w_700,h_900,g_auto/v1691703741/photo_2023-08-10_18-41-49_mnqaid.jpg"
+              alt="Natalia"
+              objectFit="cover"
+            />
+          </Box>
+          <Text
+            color={"white"}
+            textAlign="center"
+            fontSize={{ base: "lg", lg: "md" }}
+            lineHeight="1.6"
+            mt={4}
+            px={8}
+          >
+            Soy Natalia, tarotista y guía espiritual con años de experiencia en
+            el mundo del tarot. Mi pasión es ayudarte a encontrar la claridad
+            que necesitas para enfrentar situaciones, tomar decisiones y caminar
+            con confianza hacia tu destino. Con cada pregunta que hagas, te
+            brindaré interpretaciones precisas y significativas basadas en la
+            energía de los arcanos. Además, tus Cafecitos apoyarán este espacio
+            para que pueda seguir ofreciendo contenido y consejos útiles en
+            temas de desarrollo personal, espiritualidad y crecimiento interior.{" "}
+            <br />
+            {""}
+            <br />
+            Mi formación se centra en el Tarot Evolutivo una poderosa
+            herramienta que combina la antigua sabiduría del tarot con un
+            enfoque contemporáneo hacia el crecimiento personal y espiritual.
+            Este estilo de tarot va más allá de las predicciones simples y
+            ofrece una experiencia de lectura más profunda y significativa. Su
+            enfoque en la responsabilidad, la intuición y la autorreflexión lo
+            convierten en una opción única para aquellos que buscan un mayor
+            entendimiento de sí mismos y su camino en la vida. ¡Estoy aquí para
+            ti! Atrévete a dar el primer sorbo de sabiduría, y juntos
+            descubriremos el poder que el tarot puede tener en tu vida.
+          </Text>
+        </Flex>
         <Link
           href="https://cafecito.app/magicna"
           rel="noopener noreferrer"
